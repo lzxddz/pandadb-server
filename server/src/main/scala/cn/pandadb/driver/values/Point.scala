ed2fca2e4ad4ab9dafbec12e55b07d70282cfed9
MaseratiD <1140727287@qq.com>
2020-03-11 15:12:01 +0800

    update point

--- server/src/main/scala/cn/pandadb/driver/values/Point.scala
+++ server/src/main/scala/cn/pandadb/driver/values/Point.scala
@@ -1,5 +1,52 @@
 package cn.pandadb.driver.values
 
-class Point extends Serializable {
+import java.util.Objects
 
+trait Point extends Serializable{
+  def srid : Int
+  def x : Double
+  def y : Double
+  def z : Double
+}
+
+class Point2D(ss: Int, xx: Double, yy: Double) extends Point with Serializable {
+  override def srid: Int = ss
+
+  override def x: Double = xx
+
+  override def y: Double = yy
+
+  override def z: Double = Double.NaN
+
+  override def equals(o: Any): Boolean = {
+    if (this == o)
+      return true
+    if (o == null || (getClass ne o.getClass))
+      return false
+    val that = o.asInstanceOf[Point2D]
+    srid == that.srid && that.x ==  x && that.y == y
+  }
+
+  override def toString: String = "Point{" + "srid=" + srid + ", x=" + x + ", y=" + y +  '}'
+}
+
+class Point3D(ss: Int, xx: Double, yy: Double,zz: Double) extends Point with Serializable {
+  override def srid: Int = ss
+
+  override def x: Double = xx
+
+  override def y: Double = yy
+
+  override def z: Double = zz
+
+  override def equals(o: Any): Boolean = {
+    if (this == o)
+      return true
+    if (o == null || (getClass ne o.getClass))
+      return false
+    val that = o.asInstanceOf[Point3D]
+    srid == that.srid && that.x ==  x && that.y == y && that.z == z
+  }
+
+  override def toString: String = "Point{" + "srid=" + srid + ", x=" + x + ", y=" + y + ", z=" + z + '}'
 }
